4d1ed674a6b383d862c9e0abf1a105c610e7ed04
sunny4handsome@163.com <sunny4handsome@163.com>
2016-07-03 09:23:57 +0800

    change configure file

--- src/main/java/com/alibaba/middleware/race/RaceConfig.java
+++ src/main/java/com/alibaba/middleware/race/RaceConfig.java
@@ -5,9 +5,9 @@ import java.io.Serializable;
 public class RaceConfig implements Serializable {
 
     //这些是写tair key的前缀
-    public static String prex_tmall = "platformTmall_";
-    public static String prex_taobao = "platformTaobao_";
-    public static String prex_ratio = "ratio_";
+    public static String prex_tmall = "platformTmall_34871wea6u_";
+    public static String prex_taobao = "platformTaobao_34871wea6u_";
+    public static String prex_ratio = "ratio_34871wea6u_";
 
 
     //这些jstorm/rocketMq/tair 的集群配置信息，这些配置信息在正式提交代码前会被公布
@@ -17,8 +17,8 @@ public class RaceConfig implements Serializable {
     public static final String MqPayTopic = "MiddlewareRaceTestData_Pay";
     public static final String MqTmallTradeTopic = "MiddlewareRaceTestData_TMOrder";
     public static final String MqTaobaoTradeTopic = "MiddlewareRaceTestData_TBOrder";
-    public static final String TairConfigServer = "xxx";
-    public static final String TairSalveConfigServer = "xxx";
-    public static final String TairGroup = "xxx";
-    public static final Integer TairNamespace = 1;
+    public static final String TairConfigServer = "10.101.72.127:5198";
+    public static final String TairSalveConfigServer = "10.101.72.128:5198";
+    public static final String TairGroup = "group_tianchi";
+    public static final Integer TairNamespace = 57917;
 }
--- src/main/java/com/alibaba/middleware/race/jstorm/ConsumerPushFactory.java
+++ src/main/java/com/alibaba/middleware/race/jstorm/ConsumerPushFactory.java
@@ -10,7 +10,7 @@ import com.alibaba.rocketmq.common.consumer.ConsumeFromWhere;
 public class ConsumerPushFactory {
 	public static DefaultMQPushConsumer getConsumer(MessageListenerConcurrently listener){
 		DefaultMQPushConsumer consumer = new DefaultMQPushConsumer(RaceConfig.MetaConsumerGroup);
-		consumer.setNamesrvAddr(RaceConfig.MQNameServerAddr);
+		//consumer.setNamesrvAddr(RaceConfig.MQNameServerAddr);
 		consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
 		consumer.registerMessageListener(listener);
 		try {
